c6e8b360b5a7cdc2a108574d774f8effb4caa836
Tony <antony.branquinho@gmail.com>
2023-11-28 13:55:29 -0300

    facil

---
+++ Troco/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troco {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double preco, dinheiro, troco;
+            int quantidade;
+
+            Console.Write("Preço unitario do produto: ");
+            preco = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Quantidade comprada: ");
+            quantidade = int.Parse(Console.ReadLine(), CI);
+            Console.Write("Dinheiro recebido: ");
+            dinheiro = double.Parse(Console.ReadLine(), CI);
+
+            troco = dinheiro - (preco * quantidade);
+
+            Console.WriteLine("TROCO = " + troco.ToString("F2", CI));
+
+        }
+    }
+}
